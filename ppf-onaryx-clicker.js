a20d6538c883bf7446f38412dec4c7fa8121514c
pantini <johnpantini@gmail.com>
2021-03-13 23:28:46 +0300

    Add missing condition

--- ppf-onaryx-clicker.js
+++ ppf-onaryx-clicker.js
@@ -9,6 +9,8 @@ if (process.env.TG_ID && process.env.PANTINI_TOKEN) {
   });
 
   client.on('ticker', data => {
-    console.log(data);
+    if (data.m === 'ppf') {
+      console.log(data);
+    }
   });
 }
